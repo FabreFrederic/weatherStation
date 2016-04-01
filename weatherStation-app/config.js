03c6afb4a0d9e5a85ff779ebbd71cbc4427da7f2
Frederic Fabre <fabrefrederic@free.fr>
2016-04-01 15:15:44 +0200

    Display the last temperature reading in the browser - update the dependancies versions - add .gitignore file - display the last temperature reading in the browser

--- weatherStation-app/config.js
+++ weatherStation-app/config.js
@@ -1,5 +1,17 @@
 System.config({
     transpiler: 'typescript',
     typescriptOptions: { emitDecoratorMetadata: true },
+    "paths": {
+    "main": "/main",
+    "angular2/": "/angular2/",
+    "rxjs/": "/rxjs/",
+    "moment/": "/node_modules/moment/moment",
+    "": "/node_modules/",
+    "angular2-jwt": "/node_modules/angular2-jwt/angular2-jwt",
+    "ng2-charts": "/node_modules/ng2-charts"
+},
     packages: {'app': {defaultExtension: 'ts'}}
 });
+
+
+
