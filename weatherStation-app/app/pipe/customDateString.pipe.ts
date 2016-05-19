ad80e2ab8322a3ba3b35dcce0df17eec60551bcc
Frederic Fabre <fabrefrederic@free.fr>
2016-05-19 10:08:03 +0200

    Bug fix : date pipe

--- weatherStation-app/app/pipe/customDateString.pipe.ts
+++ weatherStation-app/app/pipe/customDateString.pipe.ts
@@ -3,13 +3,13 @@ import {DatePipe} from 'angular2/common';
 
 @Pipe({ name: 'customDateStringPipe' })
 export class CustomDateStringPipe implements PipeTransform {
-    transform(value: string, args: any[]): string {
+    transform(value: string, pattern: string): string {
         var parsedDate = Date.parse(value);
         if (isNaN(parsedDate)) {
             return "";
         }
         else {
-            return new DatePipe().transform(new Date(parsedDate), args);
+            return new DatePipe().transform(new Date(parsedDate), pattern);
         }
     }
 }
